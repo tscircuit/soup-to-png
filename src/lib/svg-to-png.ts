a9b05be18cf3eb3e5ccd2e74c3b49f646c4e1f2e
Rishabh Gupta <belikerishabh@gmail.com>
2024-07-26 20:40:04 +0530

    refactor: Refactor the circuitToPng function

--- src/lib/svg-to-png.ts
+++ src/lib/svg-to-png.ts
@@ -1,11 +1,9 @@
-import { promises } from 'node:fs'
-import { join } from 'node:path'
 import { Resvg } from '@resvg/resvg-js'
 import { pcbSoupToSvg, soupToSvg } from '@tscircuit/soup-to-svg'
 import type { AnySoupElement } from '@tscircuit/soup'
 
-async function soupToPng(soup: AnySoupElement[]) {
-  const svg = pcbSoupToSvg(soup)
+function circuitToPng(circuit: AnySoupElement[], circuitType: 'pcb' | 'schematic') {
+  const svg = circuitType === 'pcb' ? pcbSoupToSvg(circuit) : soupToSvg(circuit)
   const opts = {
     background: 'rgba(238, 235, 230, .9)',
     fitTo: {
@@ -17,10 +15,7 @@ async function soupToPng(soup: AnySoupElement[]) {
   const pngData = resvg.render()
   const pngBuffer = pngData.asPng()
 
-  console.info('Original SVG Size:', `${resvg.width} x ${resvg.height}`)
-  console.info('Output PNG Size  :', `${pngData.width} x ${pngData.height}`)
-
-  await promises.writeFile(join(__dirname, './output.png'), pngBuffer)
+  return pngBuffer
 }
 
-export { soupToPng }
+export { circuitToPng }
